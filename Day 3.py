70b8c77335905111ffc22e6f0e2aaccfeec40074
Tony679-max <ishimwetonysharif@gmail.com>
2026-09-09 15:30:31 +0200

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,11 @@
+score = int(input("Enter the student`s score:"))
+
+#python checks condition from top to bottom.
+if score >=80:
+    print("Excellent")
+
+elif score >= 60:
+    print("very good")
+
+else:
+    print("keep practicing") 
