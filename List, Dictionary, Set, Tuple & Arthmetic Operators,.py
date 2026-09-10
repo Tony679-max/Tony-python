af63531ecc734a86f9f15c8f1b54b3e894bb2601
Tony679-max <ishimwetonysharif@gmail.com>
2026-09-10 15:10:59 +0200

    Add files via upload

---
+++ List, Dictionary, Set, Tuple & Arthmetic Operators,.py
@@ -0,0 +1,46 @@
+
+# list
+students_names = ["Tony","Ishimwe","Sharif"]
+
+# Dictionary
+student_info = {
+    "name": "Tony",
+    "age": 20,
+    "faculty": "Software Engineering" 
+}
+
+# set
+student_modules = {"python", "C", "C++"}
+
+# tuple 
+students_marks = (85,74,70,66)
+
+print("students names:", students_names)
+print("student information:", student_info)
+print("student modules:", student_modules)
+print("students marks:", students_marks)
+
+# type conversation
+age = "10"
+age = int(age)
+
+print("Age:", age)
+
+# Arthimetic operators
+a = 20
+b = 15
+
+print("Sum:", a + b)
+print("Substraction:", a - b)
+
+#Membership test 
+if "Sharif" in students_names:
+    print("Sharif is in the student list.")
+
+
+
+
+
+
+
+
