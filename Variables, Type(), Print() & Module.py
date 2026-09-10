322bc0d191b6aae5f7535014ecefb6fe70206183
Tony679-max <ishimwetonysharif@gmail.com>
2026-09-10 15:07:08 +0200

    Add files via upload

---
+++ Variables, Type(), Print() & Module.py
@@ -0,0 +1,21 @@
+ 
+# Variables
+Student_names = "Tuyiringire Emmanuel" 
+Student_age = "25"
+Student_Faculty = "Law"
+Student_Height = "2.0"
+
+# Print() , Type()
+print((type(Student_names)) , "Student name:", Student_names) 
+print("Student age:",  Student_age , (type(Student_age)))
+print("Student faculty:",  Student_Faculty , (type(Student_Faculty)))
+print("Student height:",  Student_Height , (type(Student_Height)))
+
+# module 
+
+import math
+
+number = 0
+sine_number = math.sin(number)
+print("sine of", number, "is", sine_number)
+
