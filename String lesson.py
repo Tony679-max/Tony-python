5e95c36891eddf7680367fee4c57e9dffd186a64
Tony679-max <ishimwetonysharif@gmail.com>
2026-09-11 14:57:16 +0200

    Add files via upload

---
+++ String lesson.py
@@ -0,0 +1,37 @@
+# length of strings 
+student_name = "Computers"
+print(student_name)
+print("length:", len(student_name))
+
+text = "Hello Python"
+
+print(text[0])
+print(text[0:7])
+print(text.split())
+
+
+def greet(name):
+    print("hello", name)
+greet("john")
+
+
+def add(a,b):
+    return a + b
+result = add(5, 3)
+print(result)
+
+def add(a,b):
+    return a * b
+result = add(10, 2)
+print(result)
+
+x = 10
+def show_value():
+    y = 5
+    print(x)
+    print(y)
+show_value()
+
+
+
+
