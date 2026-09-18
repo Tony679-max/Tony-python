48cd24184aa354145040ab0fd52008db70fe3827
Tony679-max <ishimwetonysharif@gmail.com>
2026-09-18 16:36:27 +0200

    Add files via upload

---
+++ adult & minor.py.py
@@ -0,0 +1,30 @@
+# practicing python 
+
+try:
+
+    age = int(input("Enter your age:"))
+
+    if age > 105:
+     print("Maximum is 105 !")
+
+    elif age < 1:
+     print("minimum is 1 !")
+
+    else:
+
+     if age >= 18:
+        print("Adult")
+     else:
+        print("Minor")
+
+except:
+ print("ensert a number !")
+           
+     
+
+
+
+
+
+
+    
